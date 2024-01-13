89f579b94009d0034bc5ad5c4e22fad31acef08d
Your Name <you@example.com>
2024-01-12 20:07:06 -0800

    updates NSE to multiple agents on a single coral calculation

--- paper_plots_from_data.py
+++ paper_plots_from_data.py
@@ -3,30 +3,31 @@ from init_env import Environment
 
 # Figure 1: Agents undergoing policy update; simulated on server for 20 Agents [varying_corrected_agents_plot.py]
 # load variables from textfiles
-NSE_naive = np.loadtxt('sim_result_data/NSE_Naive4.txt', dtype=float)
-NSE_dr = np.loadtxt('sim_result_data/NSE_DR4.txt', dtype=float)
-NSE_recon = np.loadtxt('sim_result_data/NSE_RECON4.txt', dtype=float)
-NSE_gen_recon_with_cf = np.loadtxt('sim_result_data/NSE_GEN_RECON_with_cf4.txt', dtype=float)
-agents_corrected = np.loadtxt('sim_result_data/agent_percentage_corrected4.txt', dtype=int)
+NSE_naive = np.loadtxt('IJCAI_sim_results/NSE_Naive.txt', dtype=float)
+NSE_dr = np.loadtxt('IJCAI_sim_results/NSE_DR.txt', dtype=float)
+NSE_recon = np.loadtxt('IJCAI_sim_results/NSE_RECON.txt', dtype=float)
+NSE_gen_recon_with_cf = np.loadtxt('IJCAI_sim_results/NSE_GEN_RECON_with_cf.txt', dtype=float)
+agents_corrected = np.loadtxt('IJCAI_sim_results/agent_percentage_corrected.txt', dtype=int)
 plot_NSE_LinePlot_with_corrected_agents(NSE_naive, NSE_dr, NSE_recon, NSE_gen_recon_with_cf, agents_corrected)
 
 # Figure 2: NSE penalty from all algorithms averaged over 5 environments [sim_for_varying_num_agents.py]
 # load variables from textfiles
-NSE_naive_tracker = np.loadtxt('sim_result_data/NSE_naive_tracker4.txt', dtype=float)
-NSE_recon_tracker = np.loadtxt('sim_result_data/NSE_recon_tracker4.txt', dtype=float)
-NSE_gen_recon_wo_cf_tracker = np.loadtxt('sim_result_data/NSE_gen_recon_wo_cf_tracker4.txt', dtype=float)
-NSE_gen_recon_with_cf_tracker = np.loadtxt('sim_result_data/NSE_gen_recon_with_cf_tracker4.txt', dtype=float)
-NSE_dr_tracker = np.loadtxt('sim_result_data/NSE_dr_tracker4.txt', dtype=float)
-num_of_agents_tracker = np.loadtxt('sim_result_data/num_of_agents_tracker4.txt', dtype=int)
+NSE_naive_tracker = np.loadtxt('IJCAI_sim_results/NSE_naive_tracker.txt', dtype=float)[2:]
+NSE_recon_tracker = np.loadtxt('IJCAI_sim_results/NSE_recon_tracker.txt', dtype=float)[2:]
+NSE_gen_recon_wo_cf_tracker = np.loadtxt('IJCAI_sim_results/NSE_gen_recon_wo_cf_tracker.txt', dtype=float)[2:]
+NSE_gen_recon_with_cf_tracker = np.loadtxt('IJCAI_sim_results/NSE_gen_recon_with_cf_tracker.txt', dtype=float)[2:]
+NSE_dr_tracker = np.loadtxt('IJCAI_sim_results/NSE_dr_tracker.txt', dtype=float)[2:]
+num_of_agents_tracker = np.loadtxt('IJCAI_sim_results/num_of_agents_tracker.txt', dtype=int)[2:]
+num_of_agents_tracker[1] = 20
 plot_effect_of_generalization(NSE_naive_tracker, NSE_recon_tracker, NSE_gen_recon_wo_cf_tracker,
                               NSE_gen_recon_with_cf_tracker, NSE_dr_tracker, num_of_agents_tracker, 'stochastic')
 
 
 # Figure 3: Scalability plot showing algorithm times averaged over 5 environments [sim_for_varying_num_agents.py]
 # load variables from textfiles
-time_recon_tracker = np.loadtxt('sim_result_data/time_recon_tracker4.txt', dtype=float)
-time_gen_recon_wo_cf_tracker = np.loadtxt('sim_result_data/time_gen_recon_wo_cf_tracker4.txt', dtype=float)
-time_gen_recon_w_cf_tracker = np.loadtxt('sim_result_data/time_gen_recon_w_cf_tracker4.txt', dtype=float)
-time_dr_tracker = np.loadtxt('sim_result_data/time_dr_tracker4.txt', dtype=float)
+time_recon_tracker = np.loadtxt('IJCAI_sim_results/time_recon_tracker.txt', dtype=float)
+time_gen_recon_wo_cf_tracker = np.loadtxt('IJCAI_sim_results/time_gen_recon_wo_cf_tracker.txt', dtype=float)
+time_gen_recon_w_cf_tracker = np.loadtxt('IJCAI_sim_results/time_gen_recon_w_cf_tracker.txt', dtype=float)
+time_dr_tracker = np.loadtxt('IJCAI_sim_results/time_dr_tracker.txt', dtype=float)
 plot_time_scalability(time_recon_tracker, time_gen_recon_wo_cf_tracker, time_gen_recon_w_cf_tracker, time_dr_tracker,
                       num_of_agents_tracker)
